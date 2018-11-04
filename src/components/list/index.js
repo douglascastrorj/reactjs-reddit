d27c09ac37db70e89c8554264b2a5a08530e1c25
Douglas Castro <douglascastrorj@gmail.com>
2018-11-04 11:05:13 -0200

    lista linkando para pagina do reddit

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -7,7 +7,8 @@ class List extends Component {
 
     renderItem = items => (
         items.map((item, i) => (
-            <div key={i} className="list-item">
+
+            <div key={i} className="list-item" >
                 <div className="list-thumb">
                     {
                         item !== undefined && item.data.thumbnail !== undefined && item.data.thumbnail !== 'self' ?
@@ -23,28 +24,36 @@ class List extends Component {
 
                 </div>
 
-                <div className="list-text-content">
-                    <div className="title">
-                        {item.data.title}
-                    </div>
+                <a className="list-link"
+                
+                href={`http://reddit.com${item.data.permalink}`}
+                target="_blank"
+                >
 
-                    <div className="enviado-por">
-                        enviado em {this.formatDate(new Date(item.data.created))} <span className="nome-usuario"> {item.data.author} </span>
-                    </div>
+                    <div className="list-text-content">
+                        <div className="title">
+                            {item.data.title}
+                        </div>
 
-                    <div className="dominio">
-                        {item.data.domain}
-                    </div>
+                        <div className="enviado-por">
+                            enviado em {this.formatDate(new Date(item.data.created))} <span className="nome-usuario"> {item.data.author} </span>
+                        </div>
 
-                </div>
-            </div>
+                        <div className="dominio">
+                            {item.data.domain}
+                        </div>
+
+                    </div>
+                </a>
+            </div >
+
         ))
     )
 
     formatDate = (unix_timestamp) => {
 
         var date = new Date(unix_timestamp * 1000);
-      
+
         let day = date.getDate() <= 9 ? '0' + date.getDate() : date.getDate();
         let month = date.getMonth() < 9 ? '0' + date.getMonth() + 1 : date.getMonth() + 1;
         return `${day}/${month}/${date.getFullYear()}`;
